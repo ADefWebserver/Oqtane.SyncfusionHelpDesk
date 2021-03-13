542fab8a6e4d8b99e3ab2321ca783b7a259023ad
Michael Washington <webmaster@adefwebserver.com>
2021-03-12 17:55:47 -0800

    Start on Help Desk form

---
+++ Shared/Models/SyncfusionHelpDeskStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syncfusion.HelpDesk.Models
+{
+    public class SyncfusionHelpDeskStatus
+    {
+        public string ID { get; set; }
+        public string Text { get; set; }
+
+        public static List<SyncfusionHelpDeskStatus> Statuses =
+            new List<SyncfusionHelpDeskStatus>() {
+        new SyncfusionHelpDeskStatus(){ ID= "New", Text= "New" },
+        new SyncfusionHelpDeskStatus(){ ID= "Open", Text= "Open" },
+        new SyncfusionHelpDeskStatus(){ ID= "Urgent", Text= "Urgent" },
+        new SyncfusionHelpDeskStatus(){ ID= "Closed", Text= "Closed" },
+        };
+    }
+}
