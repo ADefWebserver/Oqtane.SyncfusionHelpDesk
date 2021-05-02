8c155093fdf4a71baf7398f337a097ab911254d1
Michael Washington <webmaster@adefwebserver.com>
2021-05-01 18:04:57 -0700

    Updated deploy paths to be standard

--- Package/release.cmd
+++ Package/release.cmd
@@ -1,2 +1,2 @@
-"..\..\Oqtane\oqtane.package\nuget.exe" pack Syncfusion.Helpdesk.nuspec 
-XCOPY "*.nupkg" "..\..\Oqtane\Oqtane.Server\wwwroot\Modules\" /Y
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack Syncfusion.Helpdesk.nuspec 
+XCOPY "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\wwwroot\Modules\" /Y
